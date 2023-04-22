e24a157df8a677782234f5772eb08d3c9959530a
dgx-1b <ilhamsyahids@gmail.com>
2023-04-22 17:30:52 +0000

    add more scheduler
    Signed-off-by: dgx-1b <ilhamsyahids@gmail.com>

--- schedulers/__init__.py
+++ schedulers/__init__.py
@@ -1,18 +1,32 @@
 
-from schedulers.lr_scheduler import CosineAnnealingWarmupRestarts, WarmupMultiStepLR
+import torch
+from schedulers.lr_scheduler import CosineAnnealingWarmupRestarts, WarmupCosineLR, WarmupMultiStepLR
 
 def get_scheduler(name: str, optimizer, cfg):
     name = name.lower()
     if name == "multisteplr":
         lr_scheduler = WarmupMultiStepLR(
-                optimizer,
-                cfg.SCHEDULER.STEPS,
-                cfg.SCHEDULER.GAMMA,
-                warmup_factor=cfg.SCHEDULER.WARMUP_FACTOR,
-                warmup_iters=cfg.SCHEDULER.WARMUP_ITERS,
-                warmup_method=cfg.SCHEDULER.WARMUP_METHOD,
-            )
+            optimizer,
+            cfg.SCHEDULER.STEPS,
+            cfg.SCHEDULER.GAMMA,
+            warmup_factor=cfg.SCHEDULER.WARMUP_FACTOR,
+            warmup_iters=cfg.SCHEDULER.WARMUP_ITERS,
+            warmup_method=cfg.SCHEDULER.WARMUP_METHOD,
+        )
     elif name == "cosineannealinglr":
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer,
+            T_max=cfg.SCHEDULER.MAX_ITER,
+        )
+    elif name == "cosineannealinglrwarmup":
+        lr_scheduler = WarmupCosineLR(
+            optimizer,
+            max_iters=cfg.SCHEDULER.MAX_ITER,
+            warmup_iters=cfg.SCHEDULER.WARMUP_ITERS,
+            warmup_factor=cfg.SCHEDULER.WARMUP_FACTOR,
+            warmup_method=cfg.SCHEDULER.WARMUP_METHOD,
+        )
+    elif name == "cosineannealinglrwarmuprestarts":
         lr_scheduler = CosineAnnealingWarmupRestarts(
             optimizer,
             first_cycle_steps=cfg.SCHEDULER.MAX_ITER, # total steps SCHEDULER.max_iter T_0
@@ -20,9 +34,21 @@ def get_scheduler(name: str, optimizer, cfg):
             gamma=cfg.SCHEDULER.GAMMA, # lr decay gamma
             warmup_steps=cfg.SCHEDULER.WARMUP_ITERS, # warmup steps T_up
         )
+    elif name == "polynomial":
+        lr_scheduler = torch.optim.lr_scheduler.PolynomialLR(
+            optimizer,
+            total_iters=cfg.SCHEDULER.MAX_ITER,
+            power=cfg.SCHEDULER.GAMMA,
+        )
     else:
         raise RuntimeError(
-            f"Invalid lr scheduler '{name}'. Only MultiStepLR and CosineAnnealingLR are supported."
+            f"Invalid lr scheduler '{name}'. \
+            Only MultiStepLR, \
+            CosineAnnealingLR, \
+            CosineAnnealingLRWarmUp, \
+            CosineAnnealingLRWarmUpRestarts, \
+            and Polynomial \
+            are supported."
         )
     
     return lr_scheduler
